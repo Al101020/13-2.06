25e83390e03d904e3db8a73b70c334e12ddf8a7b
Al101020 <alex.vl.m@yandex.ru>
2025-01-22 17:37:30 +1000

    commit-12

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,97 @@
 // TODO: write code here
+import { mdConvert } from 'md-converter';
 
 console.log('app.js bundled');
 
 const fileContainer = document.querySelector('.file-container');
 const fileInput = fileContainer.querySelector('.overlapped');
 
+const fileTitle = document.querySelector('.preview-title');
+const previewText = document.querySelector('.preview-text');
+const previewHtml = document.querySelector('.preview-html');
+const previewlmage = document.querySelector('.preview-image');
+
 fileContainer.addEventListener('click', (e) => {
   console.log(e);
 
   console.log('click');
+
+  fileInput.dispatchEvent(new MouseEvent('click'))
 });
 
-setTimeout(() => fileInput.dispatchEvent(new MouseEvent('click')), 6000);
+const displayTextConvert = (e) => {
+    console.log(e);
 
-fileInput.addEventListener('click', (e) => {
+    previewText.textContent = e.target.result;
+};
+
+const displayMdConvert = (e) => {
+    console.log(e);
+
+    previewHtml.innerHTML = mdConvert(e.target.result);
+};
+
+const displayImageConvert = (e) => {
+    console.log(e);
+    // console.log('qqq');
+    previewlmage.src = e.target.result;     // console.log(previewlmage);
+};
+
+fileInput.addEventListener('change', (e) => {
   console.log(e);
+  console.dir(fileInput);
+
+  const file = fileInput.files && fileInput.files[0];
+
+  if (!file) return;
+
+  fileTitle.textContent = file.name;
+
+  const url = URL.createObjectURL(file);
+
+  const link = document.createElement('a'); // создали ссылку
+
+  link.href = url; // добавляем необходимые атрибуты, в href кладём url
+  link.rel = 'noopener'; // обеспечивает именно скачивание, а не открытие
+  link.download = file.name; // download обеспечивает имя скаченного файла, file.name - какое имя было.
+
+  console.log(link); // на самом деле ссылка ни на странице ни в разметке не появилась(её туда не вставляли)
+  link.click(); // ну и кликним за пользователя по этой ссылки
+
+  //     // Первый способ вставки картинки:
+  // // const reader = new FileReader();
+
+  // // // // reader.addEventListener('load', displayTextConvert);
+  // // // // reader.addEventListener('load', displayMdConvert);
+
+  // // reader.addEventListener('load', displayImageConvert);
+  // // reader.readAsDataURL(file);
+
+
+  //     // Второй способ вставки картинки:
+  // const url = URL.createObjectURL(file);
+
+  // previewlmage.src = url;
+
+  // // URL.revokeObjectURL(url); // для очистки памяти - картинка не появляется на странице
+  // setTimeout(() => URL.revokeObjectURL(url), 0); // - setTimeout - даёт картинке загрузится а потом удаляет ссылку из памяти.
+
+  console.log(url);
 });
+
+
+  // const reader = new FileReader();
+
+  // // reader.addEventListener('load', displayTextConvert);
+  // // reader.addEventListener('load', displayMdConvert);
+
+  // reader.addEventListener('load', displayImageConvert);
+
+  // reader.readAsDataURL(file);
+
+
+
+
 
 // // TODO: write code here
 // import { mdConvert } from 'md-converter';
